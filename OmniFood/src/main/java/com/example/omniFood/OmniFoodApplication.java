c6bebeb4f4165bf662addd32c3338abb29688e63
tsanchit92 <tsanchit92@gmail.com>
2023-08-02 17:45:29 +0530

    adding omniFood service

---
+++ OmniFood/src/main/java/com/example/omniFood/OmniFoodApplication.java
@@ -0,0 +1,13 @@
+package com.example.omniFood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmniFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmniFoodApplication.class, args);
+	}
+
+}
